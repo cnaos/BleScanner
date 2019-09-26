d0c1a9d3471384d96d7e597b60113326939a27a2
cnaos <cnaos.gm@gmail.com>
2019-09-27 00:12:31 +0900

    メニューとスキャン処理の雛形追加

--- app/src/main/java/io/github/cnaos/blescanner/DeviceListActivity.kt
+++ app/src/main/java/io/github/cnaos/blescanner/DeviceListActivity.kt
@@ -1,10 +1,23 @@
 package io.github.cnaos.blescanner
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import io.github.cnaos.blescanner.ui.devicelist.DeviceListFragment
+import io.github.cnaos.blescanner.ui.devicelist.DeviceListViewModel
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.verbose
 
-class DeviceListActivity : AppCompatActivity() {
+class DeviceListActivity : AppCompatActivity(), AnkoLogger {
+    // 定数
+    companion object {
+        private const val REQUEST_ENABLE_BLUETOOTH = 1 // Bluetooth機能の有効化要求時の識別コード
+    }
+
+    private val viewModel: DeviceListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +27,37 @@ class DeviceListActivity : AppCompatActivity() {
                 .replace(R.id.container, DeviceListFragment.newInstance())
                 .commitNow()
         }
+
+        // スキャン状態が変わったらメニューを更新する
+        viewModel.scanning.observe(this, Observer {
+            verbose("refresh menu")
+            invalidateOptionsMenu()
+        })
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.device_list, menu)
+        if (menu == null) {
+            return true
+        }
+        if (viewModel.scanning.value!!) {
+            menu.findItem(R.id.menuitem_stop).isVisible = true
+            menu.findItem(R.id.menuitem_scan).isVisible = false
+            menu.findItem(R.id.menuitem_progress)
+                .setActionView(R.layout.actionbar_indeterminate_progress)
+        } else {
+            menu.findItem(R.id.menuitem_stop).isVisible = false
+            menu.findItem(R.id.menuitem_scan).isVisible = true
+            menu.findItem(R.id.menuitem_progress).actionView = null
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menuitem_scan -> viewModel.startDeviceScan()    // スキャンの開始
+            R.id.menuitem_stop -> viewModel.stopDeviceScan()    // スキャンの停止
+        }
+        return true
+    }
 }
--- app/src/main/java/io/github/cnaos/blescanner/ui/devicelist/DeviceListViewModel.kt
+++ app/src/main/java/io/github/cnaos/blescanner/ui/devicelist/DeviceListViewModel.kt
@@ -1,7 +1,43 @@
 package io.github.cnaos.blescanner.ui.devicelist
 
+import android.os.Handler
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.verbose
+import java.util.concurrent.TimeUnit
 
-class DeviceListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class DeviceListViewModel : ViewModel(),AnkoLogger {
+    // 定数
+    companion object {
+        private val SCAN_PERIOD: Long =
+            TimeUnit.MILLISECONDS.convert(20, TimeUnit.SECONDS)// スキャン時間
+    }
+
+    val scanning = MutableLiveData<Boolean>(false)
+
+    private val handler: Handler = Handler()
+
+    fun startDeviceScan() {
+        verbose("startDeviceScan")
+
+
+        handler.postDelayed({
+            scanning.value = false
+            // TODO BLEデバイスのスキャン停止
+        }, SCAN_PERIOD)
+
+        scanning.value = true
+        // TODO BLEデバイスのスキャン開始
+    }
+
+    fun stopDeviceScan() {
+        // 一定期間後にスキャン停止するためのHandlerのRunnableの削除
+        handler.removeCallbacksAndMessages(null)
+
+        scanning.value = false
+
+        // TODO BLEデバイスのScan停止
+    }
+
 }
