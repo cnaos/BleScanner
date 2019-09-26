318296952261682ed072f31d3f4fa853d3188886
cnaos <cnaos.gm@gmail.com>
2019-09-26 23:33:02 +0900

    プロジェクト作成

---
+++ app/src/main/java/io/github/cnaos/blescanner/MainActivity.kt
@@ -0,0 +1,19 @@
+package io.github.cnaos.blescanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.cnaos.blescanner.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
