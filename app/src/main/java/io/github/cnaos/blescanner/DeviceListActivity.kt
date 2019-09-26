655a11db2dd3f589a3c0338184a7c7d4b84a62b6
cnaos <cnaos.gm@gmail.com>
2019-09-27 00:57:44 +0900

    BLEデバイスのスキャンをできるようにした。

--- app/src/main/java/io/github/cnaos/blescanner/DeviceListActivity.kt
+++ app/src/main/java/io/github/cnaos/blescanner/DeviceListActivity.kt
@@ -1,15 +1,20 @@
 package io.github.cnaos.blescanner
 
+import android.Manifest
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.markodevcic.peko.PermissionResult
 import io.github.cnaos.blescanner.ui.devicelist.DeviceListFragment
 import io.github.cnaos.blescanner.ui.devicelist.DeviceListViewModel
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.verbose
+import org.jetbrains.anko.*
 
 class DeviceListActivity : AppCompatActivity(), AnkoLogger {
     // 定数
@@ -28,12 +33,70 @@ class DeviceListActivity : AppCompatActivity(), AnkoLogger {
                 .commitNow()
         }
 
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+        if (bluetoothAdapter == null) {
+            alert("Bluetooth Adapterが見つからないため終了します。") {
+                okButton { finish() }
+            }.show()
+        } else {
+            viewModel.bluetoothAdapter = bluetoothAdapter
+        }
+
+        // パーミッションの処理
+        setupProcessPermissionResult()
+
+
         // スキャン状態が変わったらメニューを更新する
         viewModel.scanning.observe(this, Observer {
             verbose("refresh menu")
             invalidateOptionsMenu()
         })
     }
+
+    private fun setupProcessPermissionResult() {
+        viewModel.permissionLiveData.observe(this, Observer { result: PermissionResult ->
+            verbose("process PermissionResult: $result")
+
+            when (result) {
+                is PermissionResult.Granted -> {
+                    toast("permission granted")
+                }
+                is PermissionResult.Denied.JustDenied -> {
+                    // at least one permission was denied
+                    alert("this application need permission for BLE") {
+                        okButton { }
+                    }.show()
+                }
+                is PermissionResult.Denied.NeedsRationale -> {
+                    // user clicked Deny, let's show a rationale
+                    alert("アプリを利用するには 位置情報 の権限が必要です") {
+                        okButton { }
+                    }.show()
+                }
+                is PermissionResult.Denied.DeniedPermanently -> {
+                    // Android System won't show Permission dialog anymore, let's tell the user we can't proceed
+                    alert("アプリを利用するには、アプリの権限で 位置情報 を許可してください。") {
+                        okButton { finish() }
+                    }.show()
+                }
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.isGrantedBLEPermission = isGrantedBlePermission()
+    }
+
+    fun isGrantedBlePermission(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.device_list, menu)
--- app/src/main/java/io/github/cnaos/blescanner/ui/devicelist/DeviceListViewModel.kt
+++ app/src/main/java/io/github/cnaos/blescanner/ui/devicelist/DeviceListViewModel.kt
@@ -1,10 +1,18 @@
 package io.github.cnaos.blescanner.ui.devicelist
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.os.Handler
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.markodevcic.peko.PermissionsLiveData
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.verbose
+import org.jetbrains.anko.warn
 import java.util.concurrent.TimeUnit
 
 class DeviceListViewModel : ViewModel(),AnkoLogger {
@@ -15,29 +23,69 @@ class DeviceListViewModel : ViewModel(),AnkoLogger {
     }
 
     val scanning = MutableLiveData<Boolean>(false)
+    lateinit var bluetoothAdapter: BluetoothAdapter
 
+    val permissionLiveData = PermissionsLiveData()
+    var isGrantedBLEPermission = false
     private val handler: Handler = Handler()
+
+    // デバイススキャンコールバック
+    private val mLeScanCallback = object : ScanCallback() {
+        // スキャンに成功（アドバタイジングは一定間隔で常に発行されているため、本関数は一定間隔で呼ばれ続ける）
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            verbose("BLE scanCallback:onScanResult result=$result")
+            // TODO デバイスリストに追加
+        }
+
+        override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+            verbose("BLE scanCallback:onBatchScanResults results=$results")
+            // TODO デバイスリストに追加
+        }
+    }
+
 
     fun startDeviceScan() {
         verbose("startDeviceScan")
 
+        if (!isGrantedBLEPermission) {
+            permissionLiveData.checkPermissions(Manifest.permission.ACCESS_COARSE_LOCATION)
+            return
+        }
+
+        // BluetoothLeScannerの取得
+        val scanner = bluetoothAdapter.bluetoothLeScanner
+        if (scanner == null) {
+            warn("No BluetoothLeScanner available. Is Bluetooth turned on?")
+            return
+        }
 
         handler.postDelayed({
-            scanning.value = false
-            // TODO BLEデバイスのスキャン停止
+            stopDeviceScanInner()
         }, SCAN_PERIOD)
 
         scanning.value = true
-        // TODO BLEデバイスのスキャン開始
+        // BLEデバイスのスキャン開始
+        val scanSettings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+            .build()
+
+        val scanfilters = listOf<ScanFilter>()
+        scanner.startScan(scanfilters, scanSettings, mLeScanCallback)
     }
 
     fun stopDeviceScan() {
         // 一定期間後にスキャン停止するためのHandlerのRunnableの削除
         handler.removeCallbacksAndMessages(null)
+        stopDeviceScanInner()
+    }
 
+    private fun stopDeviceScanInner() {
+        verbose("stopDeviceScan")
         scanning.value = false
 
-        // TODO BLEデバイスのScan停止
+        // BLEデバイスのScan停止
+        val scanner = bluetoothAdapter.bluetoothLeScanner ?: return
+        scanner.stopScan(mLeScanCallback)
     }
 
 }
