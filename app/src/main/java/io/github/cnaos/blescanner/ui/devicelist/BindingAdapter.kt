bc92305124e623d6c78eff2af301d186207a8240
cnaos <cnaos.gm@gmail.com>
2019-09-27 15:00:53 +0900

    identicon追加

--- app/src/main/java/io/github/cnaos/blescanner/ui/devicelist/BindingAdapter.kt
+++ app/src/main/java/io/github/cnaos/blescanner/ui/devicelist/BindingAdapter.kt
@@ -3,10 +3,16 @@ package io.github.cnaos.blescanner.ui.devicelist
 import android.graphics.Color
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.lelloman.identicon.view.ClassicIdenticonView
 
 
 @BindingAdapter("app:tint")
 fun ImageView.setTint(color: String) {
     val color = Color.parseColor(color)
     this.setColorFilter(color)
+}
+
+@BindingAdapter("app:hash")
+fun ClassicIdenticonView.setHash(hash: String) {
+    this.hash = hash.hashCode()
 }
